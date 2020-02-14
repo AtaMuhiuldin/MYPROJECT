8591c5ef9e32c09ec6657f07cdb49aa63c50c829
Umair maqbool <umairmaqbool.1181@gmail.com>
2020-02-14 19:00:07 +0500

    umair Branch

--- src/App.js
+++ src/App.js
@@ -11,12 +11,7 @@ class App extends Component {
         <div className="root">
           <Header />
           <TodoList />
-          <h1>hellow from umair 001</h1>
-          <h2>hellow from amd 001</h2>
-
-          <h2>AMD Work Day 1 002</h2>
-          <h3> umair 002 day 1</h3>
-          <h2>123 Correct Code</h2>
+        
 
 
 
